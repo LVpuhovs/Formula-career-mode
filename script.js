7ec2a8e830bb7df9d27d9c7a6aa99cd85e2cac2c
LVpuhovs <93042390+LVpuhovs@users.noreply.github.com>
2024-02-11 10:19:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,27 @@ function logout() {
     document.getElementById("logout-link").style.display = "none";
 }
 
+<script type="module">
+  // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDwJomB5vQn0uzW6Imtzj5doKpaPmFy5lI",
+    authDomain: "f1-career-mode.firebaseapp.com",
+    databaseURL: "https://f1-career-mode-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "f1-career-mode",
+    storageBucket: "f1-career-mode.appspot.com",
+    messagingSenderId: "959963427852",
+    appId: "1:959963427852:web:b215d52fed51139e306a29",
+    measurementId: "G-QSVTS91P66"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
